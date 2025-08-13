7588101c231b96dfcb9801331a01f62eacc593d1
DanielCharlo <danielhenriquecharlo@gmail.com>
2025-08-13 14:30:58 -0300

    12-08-25

--- Aula03/Exercicios.php
+++ Aula03/Exercicios.php
@@ -1,127 +1,133 @@
-<?php   
+<?php
 
-//     // Exercicio 1
- 
-//     $idade = 8;
+// Exercicio 1
 
- 
- 
-//     //  Exercicio 2
+$idade = readline("Digite sua idade: ");
+if ($idade >= 18) {
+  echo "Maior de idade";
+} else {
+  echo "Menor de idade";
+}
 
-//     $nota = 10; 
+echo "\n";
 
-// if ($nota >= 9) {
-//     echo "Excelente\n";
-// } else if ($nota >=7) {
-//     echo "bom\n";
-// } else {
-//     echo "Reprovado\n";
-// }
+//  Exercicio 2
 
-//     // Exercicio 3
+$nota = 10;
 
-//     $dia = readline("Digite um numero de 1-7 correspondente ao dia da semana: ");
+if ($nota >= 9) {
+  echo "Excelente\n";
+} else if ($nota >= 7) {
+  echo "bom\n";
+} else {
+  echo "Reprovado\n";
+}
 
-//     switch ($dia) {
-//         case "1":
-//             echo "Domingo\n";
-//             break;
-//         case "2":
-//             echo "Segunda\n";
-//             break;
-//         case "3":
-//             echo "Terça\n";
-//             break;
-//         case "4":
-//             echo "Quarta\n";
-//             break;
-//         case "5":
-//             echo "Quinta\n";
-//             break;
-//         case "6":
-//             echo "Sexta\n";
-//             break;
-//         case "7":
-//             echo "Sabado\n";
-//             break;
-//         default ;
-//         echo "Numero invalido\n";
-//     }
+// Exercicio 3
 
-//     // Exercicio 4
+$dia = readline("Digite um numero de 1-7 correspondente ao dia da semana: ");
 
-//     $numero1 = readline("Digite o primeiro numero: ");
-//     $numero2 = readline("Digite o segundo numero: ");
-//     $operacao = readline("Digite a operação: ");
+switch ($dia) {
+  case "1":
+    echo "Domingo\n";
+    break;
+  case "2":
+    echo "Segunda\n";
+    break;
+  case "3":
+    echo "Terça\n";
+    break;
+  case "4":
+    echo "Quarta\n";
+    break;
+  case "5":
+    echo "Quinta\n";
+    break;
+  case "6":
+    echo "Sexta\n";
+    break;
+  case "7":
+    echo "Sabado\n";
+    break;
+  default;
+    echo "Numero invalido\n";
+}
 
-//     switch ($operacao) {
-//         case "+":
-//             $resultado = $numero1 + $numero2;
-//             echo $resultado ,"\n";
-//             break;
-//         case "-":
-//             $resultado = $numero1 - $numero2;
-//             echo $resultado,"\n";
-//             break;
-//         case "/":
-//             $resultado = $numero1 / $numero2;
-//             echo $resultado, "\n";
-//             break;
-//         case "*":
-//             $resultado = $numero1 * $numero2;
-//             echo $resultado, "\n";
-//             break;
-//         default ;
-//         echo "Operação Invalida !\n";
-//     break;    
-//     }
+// Exercicio 4
 
-//     // Exercicio 5
+$numero1 = readline("Digite o primeiro numero: ");
+$numero2 = readline("Digite o segundo numero: ");
+$operacao = readline("Digite a operação: ");
 
-//     for($i=1;$i<=10;$i++){
-//         echo "Contagem: " , $i, "\n";
-//         }
+switch ($operacao) {
+  case "+":
+    $resultado = $numero1 + $numero2;
+    echo $resultado, "\n";
+    break;
+  case "-":
+    $resultado = $numero1 - $numero2;
+    echo $resultado, "\n";
+    break;
+  case "/":
+    $resultado = $numero1 / $numero2;
+    echo $resultado, "\n";
+    break;
+  case "*":
+    $resultado = $numero1 * $numero2;
+    echo $resultado, "\n";
+    break;
+  default;
+    echo "Operação Invalida !\n";
+    break;
+}
 
-//     // Exercicio 6
+// Exercicio 5
 
-//     $numero = readline("Digite um numero: ");
-//     for($i=$numero;$i>=1;$i--)
-//         echo $i , "\n";
+for ($i = 1; $i <= 10; $i++) {
+  echo "Contagem: ", $i, "\n";
+}
 
-//     // Exercicio 7
+// Exercicio 6
 
-//   $pares = 0;
-  
-//   $num = readline("Digite até que numero voce quer os pares: ");
-//   for($i=0;$i<$num+1;$i++){
-//     if($i%2==0){
-//         $pares+=1;
-//     }
-//   }
+$numero = readline("Digite um numero: ");
+for ($i = $numero; $i >= 1; $i--)
+  echo $i, "\n";
 
-//   echo "Entre 0 e $num existem $pares \n";
-  
-//   // Exercicio 8
+// Exercicio 7
 
-//   $num = readline("Digite um numero: ");
-//   for($i=1;$i<=10;$i++){
-//     $resultado = $num * $i;
-//     echo $resultado , "\n";
-//   }  
+$pares = 0;
 
-//   // Exercicio 9
+$num = readline("Digite até que numero voce quer os pares: ");
+for ($i = 0; $i < $num + 1; $i++) {
+  if ($i % 2 == 0) {
+    $pares += 1;
+  }
+}
 
-//   $temp = readline("Digite a temperatura: ");
-//   if ($temp <=15) {
-//     echo "Frio\n";
-//   } else if($temp <=25) {
-//     echo "Agradavel\n";
-//   } else {
-//     echo "Quente\n";
-//   }
+echo "Entre 0 e $num existem $pares \n";
 
-  // Exercicio 10
+// Exercicio 8
 
+$num = readline("Digite um numero: ");
+for ($i = 1; $i <= 10; $i++) {
+  $resultado = $num * $i;
+  echo $resultado, "\n";
+}
+
+// Exercicio 9
+
+$temp = readline("Digite a temperatura: ");
+if ($temp <= 15) {
+  echo "Frio\n";
+} else if ($temp <= 25) {
+  echo "Agradavel\n";
+} else {
+  echo "Quente\n";
+}
+
+// Exercicio 10
+
+do {
   echo "1- Olá\n";
   echo "2- Data Atual\n";
   echo "3- Sair\n";
@@ -130,17 +136,26 @@
 
   switch ($op) {
     case "1":
-        echo "Olá\n";
-        break;
+      echo "Olá\n";
+      break;
     case "2":
-        $data_atual = date("d/m/Y"); 
-        echo "Data atual: " . $data_atual , "\n"; 
-        break;
+      $data_atual = date("d/m/Y");
+      echo "Data atual: " . $data_atual, "\n";
+      break;
     case "3";
-        echo "Saindo...\n";
-        break;
-        default:
-        echo "opção invalida\n";
-        break;
+      echo "Saindo...\n";
+      break;
+    default:
+      echo "opção invalida\n";
+      break;
   }
-?>
+
+  echo "\n";
+} while ($op != 3);
+
+// Desafio Extra
+
+$valor1 = readline("Digite um caracter: ");
+$valor2 = readline("Digite outro caracter: ");
+
+echo "O tipo de variavel do caracter 1 é: ", gettype($valor1), "\no segundo é: ", gettype($valor2);
